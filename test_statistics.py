eb94143e18541880bbe5f9bfb0437ac7ccb8cf35
kulisarawiangin <kulisara.w@ku.th>
2022-10-12 11:43:04 +0700

    fix yml

--- test_statistics.py
+++ test_statistics.py
@@ -6,9 +6,9 @@ class StatisticsTest(TestCase):
 
     def test_variance_typical_values(self):
         """variance of typical values"""
-        self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
-        self.assertEqual(2.0, variance([1,2,3,4,5]))
-        self.assertEqual(8.0, variance([10,2,8,4,6]))
+        self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
+        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
+        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
 
     def test_variance_throws_exception(self):
         """variance of an empty list should raise an exception"""
@@ -16,10 +16,11 @@ class StatisticsTest(TestCase):
             var = variance([])
 
     def test_stdev(self):
-        # standard deviation is zero if all same
+        """test values for standard deviation."""
+        # standard deviation is zero
         data = [10.0, 10.0]
-        self.assertEqual(2.0, stdev(data))
-        #variance is 4 std.dev is sqrt(2)
+        self.assertEqual(0.0, stdev(data))
+        # standard deviation is 2.0
         data = [10.0, 14.0]
         self.assertEqual(2.0, stdev(data))
 
@@ -27,4 +28,3 @@ class StatisticsTest(TestCase):
 if __name__ == '__main__':
     import unittest
     unittest.main(verbosity=1)
-
